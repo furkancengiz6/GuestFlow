2ef7b2836add334586a40e472dfc524ca128b2c7
Furkan Cengiz <76488131+furkancengiz6@users.noreply.github.com>
2025-04-08 18:14:28 +0300

    Update YachtTourEntity.cs

--- GuestFlow.Domain/Entities/Core/YachtTourEntity.cs
+++ GuestFlow.Domain/Entities/Core/YachtTourEntity.cs
@@ -1,4 +1,4 @@
-﻿using GuestFlow.Domain.Entities.Interfaces;
+using GuestFlow.Domain.Entities.Interfaces;
 using GuestFlow.Domain.Entities.Operations;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore;
@@ -33,7 +33,15 @@ namespace GuestFlow.Domain.Entities.Core
         public override void Configure(EntityTypeBuilder<YachtTourEntity> builder)
         {
             base.Configure(builder);
-          
+            builder.Property(yt => yt.SpecialRequest).HasMaxLength(1000);
+            builder.Property(yt => yt.YachtName).HasMaxLength(100);
+            builder.Property(yt => yt.Price).HasPrecision(18, 2);
+            builder.Property(yt => yt.DiscountPercentage).HasPrecision(5, 2);
+            builder.Property(yt => yt.FinalPrice).HasPrecision(18, 2);
+            builder.HasOne(yt => yt.OwnerGuest)
+               .WithMany(g => g.YachtTours) 
+               .HasForeignKey(yt => yt.OwnerGuestId)
+               .OnDelete(DeleteBehavior.NoAction);
         }
     }
 }
