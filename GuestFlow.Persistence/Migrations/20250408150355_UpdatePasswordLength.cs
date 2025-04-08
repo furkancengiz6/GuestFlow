8cde67ec561fdf8d0b526eb06237516b10aec357
Furkan Cengiz <76488131+furkancengiz6@users.noreply.github.com>
2025-04-08 18:16:27 +0300

    Add files via upload

---
+++ GuestFlow.Persistence/Migrations/20250408150355_UpdatePasswordLength.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GuestFlow.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePasswordLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Personnels",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.UpdateData(
+                table: "Settings",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 4, 8, 18, 3, 54, 588, DateTimeKind.Local).AddTicks(7720));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Personnels",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256);
+
+            migrationBuilder.UpdateData(
+                table: "Settings",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 4, 8, 18, 0, 45, 573, DateTimeKind.Local).AddTicks(5843));
+        }
+    }
+}
